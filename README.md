d55b9fa71308fdf28ff335ec8f363d290605199c
Chastity Blackwell <cblkwell@truss.works>
2019-11-20 15:44:18 -0600

    Updating for terraform 0.12

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 Creates an AWS Lambda function to clean up manual RDS snapshots
 on a scheduled interval using [truss-aws-tools](https://github.com/trussworks/truss-aws-tools).
 
 Creates the following resources:
 
-* IAM role for Lambda function find and delete expired RDS snapshots for a
+  IAM role for Lambda function find and delete expired RDS snapshots for a
   defined RDS instance.
 * CloudWatch Event to trigger Lambda function on a schedule.
 * AWS Lambda function to actually delete excess manual RDS snapshots.
+
+## Terraform Versions
+
+Terraform 0.12: Pin module version to ~> 2.0.0. Submit pull requests to master branch.
+Terraform 0.11: Pin module version to ~> 1.0.0. Submit pull requests to terraform011 branch.
 
 ## Usage
 
@@ -28,6 +32,7 @@ module "rds-snapshot-cleaner" {
 }
 ```
 
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 ## Inputs
 
 | Name | Description | Type | Default | Required |
--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
 variable "cleaner_db_instance_identifier" {
   description = "The RDS database instance identifier."
-  type        = "string"
+  type        = string
 }
 
 variable "cleaner_dry_run" {
   description = "Don't make any changes and log what would have happened."
-  type        = "string"
+  type        = string
 }
 
 variable "cleaner_max_db_snapshot_count" {
   description = "The maximum number of manual snapshots allowed. This takes precedence over -retention-days."
-  type        = "string"
+  type        = string
 }
 
 variable "cleaner_retention_days" {
   description = "The maximum retention age in days."
-  type        = "string"
+  type        = string
 }
 
 variable "cloudwatch_logs_retention_days" {
   default     = 90
   description = "Number of days to keep logs in AWS CloudWatch."
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
@@ -31,15 +31,16 @@ variable "environment" {
 variable "interval_minutes" {
   default     = 5
   description = "How often to run the Lambda function in minutes."
-  type        = "string"
+  type        = string
 }
 
 variable "s3_bucket" {
   description = "The name of the S3 bucket used to store the Lambda builds."
-  type        = "string"
+  type        = string
 }
 
 variable "version_to_deploy" {
   description = "The version the Lambda function to deploy."
-  type        = "string"
+  type        = string
 }
+
